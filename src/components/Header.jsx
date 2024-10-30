f9cb234b16cab370e998908eec5ff153d88b492c
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-10-30 16:41:15 -0400

    structure recomposition and using createContext and useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
       <header id="main-header">
         <div id="main-title">
           <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
+          <h1>Designer Shopping</h1>
         </div>
         <p>
           <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
